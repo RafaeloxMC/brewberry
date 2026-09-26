13d7e53563a30b153da88f38cb2a51838d488c02
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-09-26 20:28:30 +0200

    Fix order area accepting wrong drinks

--- scripts/order_area.gd
+++ scripts/order_area.gd
@@ -6,12 +6,10 @@ func _process(_delta: float) -> void:
 			if area is not Cup:
 				continue
 			if area.size == GameManager.current_order_size:
-				if GameManager.current_order_foam && !(area.get_child(0).animation.contains("_foam") || area.get_child(0).animation.contains("_fill")):
-					print("No foam")
-					return
-				if GameManager.current_order_straw && !area.get_child(0).animation.contains("_straw"):
-					print("No straw")
-					return
+				if GameManager.current_order_foam != (area.get_child(0).animation.contains("_foam") || area.get_child(0).animation.contains("_fill")):
+					continue
+				if GameManager.current_order_straw != area.get_child(0).animation.contains("_straw"):
+					continue
 				var val = 1 if area.size == "sm" else 3 if area.size == "md" else 5
 				GameManager.sell.emit(val)
 				area.queue_free()
